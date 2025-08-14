f24b4ddffbc41e50a0b42a42f0caaafc2c721952
이태훈 <139675702+xoxohoon01@users.noreply.github.com>
2025-08-14 11:14:15 +0900

    init: 프로젝트 생성

---
+++ src/main/java/com/oneonefive/PathNote/PathNoteApplication.java
@@ -0,0 +1,13 @@
+package com.oneonefive.PathNote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PathNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PathNoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oneonefive/PathNote/PathNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oneonefive.PathNote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PathNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
