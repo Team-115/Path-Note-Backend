3c1c46d39c21f1919f8c93ca74e2fac80a9084f2
이태훈 <139675702+xoxohoon01@users.noreply.github.com>
2025-08-20 10:17:12 +0900

    Fix: PlaceService의 import할 패키지 수정

--- src/main/java/com/oneonefive/PathNote/service/PlaceService.java
+++ src/main/java/com/oneonefive/PathNote/service/PlaceService.java
@@ -6,8 +6,8 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.oneonefive.PathNote.entity.Course;
-import com.oneonefive.PathNote.repository.CourseRepository;
+import com.oneonefive.PathNote.entity.Place;
+import com.oneonefive.PathNote.repository.PlaceRepository;
 
 @Service
 public class PlaceService {
