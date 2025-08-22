9def80fd5466128db29dcdfa720d1f33c3a9a5b8
이태훈 <139675702+xoxohoon01@users.noreply.github.com>
2025-08-22 15:14:58 +0900

    Refactor: User 엔티티 메소드 추가

--- src/main/java/com/oneonefive/PathNote/entity/User.java
+++ src/main/java/com/oneonefive/PathNote/entity/User.java
@@ -21,6 +21,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Table(name = "users")
 public class User {
+    
     // 회원 ID
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -30,14 +31,29 @@ public class User {
     // 카카오톡 ID
     @Column(name = "kakao_id")
     private String kakaoId;
+  
     // 닉네임
     @Column(name = "nickname")
     private String nickname;
+  
     // 계정 생성 시간
     @Column(name = "created_at")
     private LocalDateTime createdAt;
+  
     // 프로필 이미지
     @Column(name = "profile_preset")
     private String profilePreset;
 
+    // 닉네임, 프로필 업데이트 메서드
+    public User update(String nickname, String profilePreset) {
+        this.nickname = nickname;
+        this.profilePreset = profilePreset;
+        return this;
+    }
+
+    // 닉네임 업데이트 메서드
+    public User update(String nickname) {
+        this.nickname = nickname;
+        return this;
+    }
 }
